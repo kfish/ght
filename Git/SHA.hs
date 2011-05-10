1d21df4dc243d5097fa9de5dd17a3a623e943777
Conrad Parker <conrad@metadecks.org>
2011-05-10 14:42:33 +0900

    SHA: add readDigestBS
    A simple function to parse a hex string into a ByteString representing
    a SHA digest.

--- Git/SHA.hs
+++ Git/SHA.hs
@@ -1,13 +1,16 @@
 {-# OPTIONS -Wall #-}
 
 module Git.SHA (
-    showDigestBS
+    showDigestBS,
+    readDigestBS
 ) where
 
 import Data.Bits
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Data.Char
+import Data.List (unfoldr)
+import Numeric
 
 ------------------------------------------------------------
 -- From Data.Digest.Pure.SHA
@@ -20,3 +23,9 @@ showDigestBS bs = foldr paddedShowHex [] (BS.unpack bs)
                       : intToDigit (fromIntegral (x .&. 0xf))
                       : xs
 
+
+------------------------------------------------------------
+-- Read a string as a hex bytestring
+
+readDigestBS :: String -> ByteString
+readDigestBS = BS.pack . map (fst . head . readHex) . takeWhile (not . null) . unfoldr (Just . splitAt 2)
